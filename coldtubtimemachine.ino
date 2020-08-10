32fd23e0f57ceeb4ab781cc3b5e11e30cc95d0e1
Andrew Ralon <andrew.ralon@golfchannel.com>
2020-08-10 10:28:45 -0400

    Update coldtubtimemachine.ino

--- coldtubtimemachine.ino
+++ coldtubtimemachine.ino
@@ -1,5 +1,10 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 #define GETNAMEOF(name) getnameof(#name, (name))
 #define ONE_WIRE_BUS 2			// Data wire is plugged into Arduino port 2
@@ -19,14 +24,66 @@ const int timeDelay = 1000;
 float tempC = 0;
 float tempF = 0;
 
+// Function to get the name of a variable
+void getnameof(char *name, int value) // char[] ???
+{
+	// Serial.print("name: %s\tvalue: %d\n", name, value);
+	Serial.print(name);
+}
+
+// Function to show device address
+void printAddress(DeviceAddress deviceAddress)
+{
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		// Pad the address with zeroes if necessary
+		if (deviceAddress[i] < 16)
+			Serial.print("0");
+		Serial.print(deviceAddress[i], HEX);
+	}
+}
+
+// Function to print the temperature for a device
+void printTemperature(DeviceAddress deviceAddress)
+{
+	float tempC = sensors.getTempC(deviceAddress);
+	float tempF = DallasTemperature::toFahrenheit(tempC);
+
+	lcd.home();
+
+	if (tempC < 10)
+	{
+		Serial.print(" ");
+		lcd.print(" ");
+	}
+	Serial.print(tempC);
+	lcd.print(tempC);
+	Serial.print(" C / ");
+	lcd.print(" C");
+	lcd.setCursor(0, 1);
+
+	if (tempF < 10)
+	{
+		Serial.print(" ");
+		lcd.print(" ");
+	}
+	Serial.print(tempF);
+	lcd.print(tempF);
+	Serial.print(" F");
+	lcd.print(" F");
+}
+
 // Setup - Initialize the sensors here
 void setup(void)
 {
 	Serial.println("O HAI");
 
-	// Start serial port and library
+	// Start serial port, sensors, and LCD
 	Serial.begin(9600);
 	sensors.begin();
+	lcd.begin(16, 2);
+	lcd.init();		 //initialize the lcd
+	lcd.backlight(); //open the backlight
 
 	// Get all devices on the bus
 	Serial.print("Locating devices... ");
@@ -74,41 +131,6 @@ void setup(void)
 	}
 }
 
-// Function to get the name of a variable
-void getnameof(char *name, int value) // char[] ???
-{
-	// Serial.print("name: %s\tvalue: %d\n", name, value);
-	Serial.print(name);
-}
-
-// Function to show device address
-void printAddress(DeviceAddress deviceAddress)
-{
-	for (uint8_t i = 0; i < 8; i++)
-	{
-		// Pad the address with zeroes if necessary
-		if (deviceAddress[i] < 16)
-			Serial.print("0");
-		Serial.print(deviceAddress[i], HEX);
-	}
-}
-
-// Function to print the temperature for a device
-void printTemperature(DeviceAddress deviceAddress)
-{
-	float tempC = sensors.getTempC(deviceAddress);
-	float tempF = DallasTemperature::toFahrenheit(tempC);
-
-	if (tempC < 10)
-		Serial.print(" ");
-	Serial.print(tempC);
-	Serial.print(" C / ");
-	if (tempF < 10)
-		Serial.print(" ");
-	Serial.print(tempF);
-	Serial.print(" F");
-}
-
 // Main - Get and show temperatures
 void loop(void)
 {
@@ -116,6 +138,8 @@ void loop(void)
 	Serial.print("Requesting temperatures.... ");
 	sensors.requestTemperatures();
 	Serial.println("Done.");
+
+	lcd.clear();
 
 	// Show the device info
 	for (int index = 0; index < deviceCount; index++)
